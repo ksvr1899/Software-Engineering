b50f2b216a2f8108adcc94ee4c230b477aaf6969
ksvr1899 <99055638+ksvr1899@users.noreply.github.com>
2022-05-04 12:20:41 -0500

    3rd Requirement
    Source files

--- operations.py
+++ operations.py
@@ -2,40 +2,45 @@
 import re
 import nltk
 
-#tokenizing the entire data
+
+# tokenizing the entire data
 def tokenize(lines):
-    """Tokenizing the entire data"""
-    return re.sub(r" [^\w] " , " ", lines).split()
-#Counting the words
+    return re.sub(r" [^\w] ", " ", lines).split()
+
+
+# counting the words
 def count(words):
-    """Counting the words"""
     return nltk.FreqDist(words)
-#word replacment in the file
-def replaceword( file_x, searchword, repword):
+
+
+# word replacment in the file
+def replaceword(file_x, searchword, repword):
     """Repalcing the word in the file"""
     with open(file_x, 'r') as file:
         filedata = file.read()
-    split_list = re.split(" ",filedata)
-    #splitting the text utilizing spaces into a rundown
+    split_list = re.split(" ", filedata)
+    # splitting the text utilizing spaces into a rundown
     for i in range(len(split_list)):
         if searchword == split_list[i]:
-            split_list[i]=repword
-    filedata=" ".join(split_list)
+            split_list[i] = repword
+    filedata = " ".join(split_list)
     # Writing the replaced data in our
     # text file
     with open(file_x, 'w') as file:
         file.write(filedata)
-    print("text Replaced done") #printing the new string
-#grep function
-def grepline(file_x,grep_word):
+    print("text Replaced done")  # printing the new string
+
+
+# grep function
+def grepline(file_x, grep_word):
     """Grep line Function"""
-    split_list=[]
-    grep_lines=[]
-    with open(file_x, 'r') as file :
+    split_list = []
+    grep_lines = []
+    with open(file_x, 'r') as file:
         filedata = file.readlines()
 
     for i in filedata:
-        split_list=i.split()
+        split_list = i.split()
         if grep_word in split_list:
             grep_lines.append(i)
     return grep_lines
